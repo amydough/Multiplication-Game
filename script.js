b7fcf37b9a624aa8e1087ba4b6904358ac7b31d1
Amy Dougherty <amdoughe@gmail.com>
2022-03-14 22:17:57 -0400

    created working display and check answer buttons

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-let x = Math.floor(Math.random() * 13);
-let y = Math.floor(Math.random() * 13);
+x = 0
+y = 0
 
-document.getElementById("num1").innerHTML = x;
-document.getElementById("num2").innerHTML = y;
+// document.getElementById("num1").innerHTML = x;
+// document.getElementById("num2").innerHTML = y;
 
-// displayMath(x, y);
 
-function playGame(a, b) {
+function displayMath() {
+    x = Math.floor(Math.random() * 13);
+    y = Math.floor(Math.random() * 13);
+    document.getElementById("num1").innerHTML = x;
+    document.getElementById("num2").innerHTML = y;
+    document.getElementById("answ").innerHTML = " ";
+    return x, y;
+}
+
+function checkAns(a, b) {
     // let a = Math.floor(Math.random() * 13);
     // let b = Math.floor(Math.random() * 13);
     document.getElementById("answ").innerHTML = a * b;
 }
-
-// function displayMath(a, b) {
-//     document.getElementById("num1").innerHTML = a;
-//     document.getElementById("num2").innerHTML = b;
-// }
