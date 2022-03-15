c93dec53e53773ebdada696554219753a30bff49
Amy Dougherty <amdoughe@gmail.com>
2022-03-14 21:50:30 -0400

    numbers display automatically; answer displayed after button click

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-// let x = Math.floor(Math.random() * 13);
-// let y = Math.floor(Math.random() * 13);
+let x = Math.floor(Math.random() * 13);
+let y = Math.floor(Math.random() * 13);
 
-// document.getElementById("myBtn").addEventListener("click", function () {
-//     playGame(x, y);
-// });
+document.getElementById("num1").innerHTML = x;
+document.getElementById("num2").innerHTML = y;
 
-function playGame() {
-    let a = Math.floor(Math.random() * 13);
-    let b = Math.floor(Math.random() * 13);
-    document.getElementById("num1").innerHTML = a;
-    document.getElementById("num2").innerHTML = b;
-    // document.getElementById("answ").innerHTML = a * b;
+// displayMath(x, y);
+
+function playGame(a, b) {
+    // let a = Math.floor(Math.random() * 13);
+    // let b = Math.floor(Math.random() * 13);
+    document.getElementById("answ").innerHTML = a * b;
 }
+
+// function displayMath(a, b) {
+//     document.getElementById("num1").innerHTML = a;
+//     document.getElementById("num2").innerHTML = b;
+// }
